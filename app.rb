71914e2696c165f0b946a4faf877be5745b4acc3
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-21 03:56:29 +0000

    Done.

--- app.rb
+++ app.rb
@@ -5,7 +5,11 @@ module FormsLab
 
     # code other routes/actions here
     get '/' do
-      erb :new
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
     end
 
     post '/pirates' do
@@ -16,7 +20,7 @@ module FormsLab
       end
 
       @ships = Ship.all
-      erb :show
+      erb :'pirates/show'
     end
 
   end
